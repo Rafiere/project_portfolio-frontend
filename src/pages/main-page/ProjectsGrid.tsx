e54e34fe1bad9f0e26ae53f22735521b8bb3e7d6
Rafiere <rafapgbr@gmail.com>
2023-03-23 10:16:30 -0300

    feat: add main grid component

---
+++ src/pages/main-page/ProjectsGrid.tsx
@@ -0,0 +1,14 @@
+import {Box, Grid} from "@chakra-ui/react";
+
+const ProjectsGrid = () => {
+    return (
+        <Grid>
+            {/* Caixinha do Projeto */}
+            <Box>
+
+            </Box>
+        </Grid>
+    )
+}
+
+export default ProjectsGrid
