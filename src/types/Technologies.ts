5336ee8b2453c3169d8da5e9065365ad241248c3
Rafiere <rafapgbr@gmail.com>
2023-03-23 10:16:51 -0300

    feat: add techonologies skeleton

---
+++ src/types/Technologies.ts
@@ -0,0 +1,28 @@
+export interface TechnologyProps {
+    name: string
+    color: string
+}
+
+export const html: TechnologyProps = {
+    name: "HTML 5",
+    color: "#E56027"
+}
+
+export const css: TechnologyProps = {
+    name: "CSS",
+    color: "#30A8D8"
+}
+
+export const java: TechnologyProps = {
+    name: "Java",
+    color: "#D3455B"
+}
+
+export const javascript: TechnologyProps = {
+    name: "JavaScript",
+    color: "#F8DC3E"
+}
+export const typescript: TechnologyProps = {
+    name: "TypeScript",
+    color: "#3178C6"
+}
