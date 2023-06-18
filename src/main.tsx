79e9007a203fcd9aa68a315219dc1d4644931741
Rafiere <rafapgbr@gmail.com>
2023-06-18 15:06:59 -0300

    feat: add Chakra UI theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './shared/config/chakra-ui/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
 );
